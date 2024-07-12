103bbcf88468285da843321084cf2d710afb5de0
Emilia <emilia.ferlin@sbsistemas.com.br>
2024-07-11 22:17:36 -0300

    feat: adiciona maven e adiciona as configuracoes iniciais

---
+++ Estacione/src/main/java/br/edu/ifsul/cc/lpoo/estacione/Estacione.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.ifsul.cc.lpoo.estacione;
+
+/**
+ *
+ * @author Mili
+ */
+public class Estacione {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
